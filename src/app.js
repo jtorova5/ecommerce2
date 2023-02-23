8905c38998b72f440bf9fa1e11d58fb6cb8ac5f8
Jhonatan Toro Vásquez <109235605+jtorova5@users.noreply.github.com>
2023-02-22 19:54:28 -0500

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 
 const express = require('express');
 const handlebars = require('express-handlebars');
-//const productsRoute = require('./routes/products.routes');
-//const cartsRoute = require ('./routes/carts.routes');
 const productsRouteDB = require('./routes/products.routes.db');
 const cartsRouteDB = require('./routes/carts.routes.db');
 const viewRoute = require('./routes/views.routes');
 const chatsRoutes = require('./routes/chats.routes');
 const server = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoConnect = require('connect-mongo');
+const routerViews = require('./routes/views.route');
+const routerSession = require('./routes/session.router');
 
 mongoose.set('strictQuery', false)
 
@@ -28,19 +30,36 @@ server.use(express.static(__dirname + '/public'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+//session cookies
+server.use(session({
+    store: MongoConnect.create({
+        mongoUrl: 'mongodb+srv://admin:123@codercluster.ew29ctl.mongodb.net/?retryWrites=true&w=majority',
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+    }),
+    secret: 'secretpassword',
+    resave: true,
+    saveUninitialized: true,
+}))
+
 //routes
-// server.use("/api/products/", productsRoute);
-// server.use("/api/carts/", cartsRoute);
+server.use(routerViews);
+server.use("/api/session", routerSession);
+
+//routes mondb
 server.use("/", viewRoute);
-server.use("/api/productsBd/", productsRouteDB );
-server.use("/api/cartsBd/", cartsRouteDB );
-server.use("/api/chats/", chatsRoutes );
+server.use("/api/productsBd/", productsRouteDB);
+server.use("/api/cartsBd/", cartsRouteDB);
+server.use("/api/chats/", chatsRoutes);
 
 //Connection with MongoDB Atlas Database
 const test = async () => {
-    await mongoose.connect ('mongodb+srv://admin:123@codercluster.ew29ctl.mongodb.net/?retryWrites=true&w=majority',
-   );
-  console.log('Se ah conectado a la base de datos correctamente');
-  };
-  
-  test();
+    await mongoose.connect('mongodb+srv://admin:123@codercluster.ew29ctl.mongodb.net/?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
+    console.log('Successfully connected with MongoDB Atlas');
+};
+
+test();
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -4,8 +4,11 @@ const viewControllers = require('../controller/views.controller')
 
 const router = Router();
 
-router.get('/products/', viewControllers.views);
+router.get('/products', viewControllers.views);
 router.get('/carts/:cid', viewControllers.viewCart);
-
+router.get('/', viewControllers.login);
+router.get('/register', viewControllers.register);
+router.get('/profile', viewControllers.profile);
+router.get('/logout', viewControllers.logout);
 
 module.exports = router;
